2743391e8c44799fbd2bc0b390a84513f3fdc243
lIlKyMalIl <kyma9965@gmail.com>
2016-07-25 22:28:08 +0300

    Update assignment10.ino

--- assignment10.ino
+++ assignment10.ino
@@ -3,13 +3,14 @@
 SR04 sensor1, sensor2;
 const int TRIGGER_1 = 6; // Χρησιμοποιήστε τα pin που θέλετε
 const int ECHO_1 = 7; 
-const int TRIGGER_2 = 5;
-const int ECHO_2 = 4;
+const int TRIGGER_2 = A2;
+const int ECHO_2 = A3;
 
 void setup() {
   Serial.begin(9600);
+  sensor1.attach(TRIGGER_1,ECHO_1);
+  sensor2.attach(TRIGGER_2,ECHO_2);
 }
 
 void loop() {
-
 }
