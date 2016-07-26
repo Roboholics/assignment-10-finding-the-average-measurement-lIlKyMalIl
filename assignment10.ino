484c2d35c714fdebdb83c378fc25ef279a5e02fd
lIlKyMalIl <kyma9965@gmail.com>
2016-07-26 12:50:08 +0300

    Update assignment10.ino

--- assignment10.ino
+++ assignment10.ino
@@ -6,75 +6,35 @@ const int ECHO_1 = 7;
 const int TRIGGER_2 = A2;
 const int ECHO_2 = A3;
 
-// Define the number of samples to keep track of.  The higher the number,
-// the more the readings will be smoothed, but the slower the output will
-// respond to the input.  Using a constant rather than a normal variable lets
-// use this value to determine the size of the readings array.
-const int numReadings = 10;
-
-int readings[numReadings];      // the readings from the analog input
-int readIndex = 0;              // the index of the current reading
-int total = 0;                  // the running total
-int average = 0;                // the average
-
-int inputPin1 = 6; 
-int inputPin2 = A2;
 
 void setup() {
   Serial.begin(9600);
   sensor1.attach(TRIGGER_1,ECHO_1);
   sensor2.attach(TRIGGER_2,ECHO_2);
-  
-  // initialize all the readings to 0:
-  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
-  readings[thisReading] = 0;
-  }
-}
+  int sum1 = 0;
+  int sum2 = 0;
+  int counter1 = 0;
+  int counter2 = 0;
+}  
 
 void loop() {
-
- // subtract the last reading:
-  total = total - readings[readIndex];
-  // read from the sensor:
-  readings[readIndex] = analogRead(inputPin1);
-  // add the reading to the total:
-  total = total + readings[readIndex];
-  // advance to the next position in the array:
-  readIndex = readIndex + 1;
-
-  // if we're at the end of the array...
-  if (readIndex >= numReadings) {
-    // ...wrap around to the beginning:
-    readIndex = 0;
-  }
-
-  // calculate the average:
-  average = total / numReadings;
-  // send it to the computer as ASCII digits
-  Serial.println(average);
-  delay(1);        // delay in between reads for stability
+  int Distance1 = sensor1.getDistance;
+  int Distance2 = sensor2.getDistance;
   
-   // subtract the last reading:
-  total = total - readings[readIndex];
-  // read from the sensor:
-  readings[readIndex] = analogRead(inputPin2);
-  // add the reading to the total:
-  total = total + readings[readIndex];
-  // advance to the next position in the array:
-  readIndex = readIndex + 1;
-
-  // if we're at the end of the array...
-  if (readIndex >= numReadings) {
-    // ...wrap around to the beginning:
-    readIndex = 0;
+  if (Distance1 > 0){
+    sum1 = sum1 + Distance1;
+    counter1 = counter1 + 1;
   }
-
-  // calculate the average:
-  average = total / numReadings;
-  // send it to the computer as ASCII digits
-  Serial.println(average);
-  delay(1);        // delay in between reads for stability
+  if (counter1 >= 10){
+    sum1 = sum1 / 10; 
+  }
+   
+  if (Distance2 > 0){
+    sum2 = sum2 + Distance2;
+    counter2 = counter2 + 1;
+  }
+  if (counter2 >= 10){
+    sum2 = sum2 / 10; 
+  }
 }
- 
-
  
